91ee8e56362ebc7fbf02b5a74c532f28b0eb9c70
Preston Chaplin <preston.chaplin@gmail.com>
2019-08-01 11:04:34 -0400

    gold cube and point lights

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-let renderer, camera, controls, scene, cubeOne, cubeTwo, cubeThree;
+let renderer, camera, controls, scene, cubeOne;
 
 class App extends Component {
   componentDidMount() {
@@ -41,91 +41,71 @@ class App extends Component {
     // light.position.set(5, 5, 5);
     // scene.add(light);
 
-    // const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    // const ambientLight = new THREE.AmbientLight(0xffffff, 1);
     // scene.add(ambientLight);
 
     const sphere = new THREE.SphereBufferGeometry(0.5, 16, 8);
 
     //Green
-    const pointLight = new THREE.PointLight(0x1bc236, 1);
-    pointLight.position.x = 800;
-    pointLight.position.z = 300;
+    // const pointLight = new THREE.PointLight(0x1bc236, 1);
+    const pointLight = new THREE.PointLight(0xfff1b8, 1.5);
+    pointLight.position.x = 8;
+    pointLight.position.z = 0;
     pointLight.add(
       new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0x1bc236 }))
     );
     scene.add(pointLight);
 
     //Red
-    const pointLight2 = new THREE.PointLight(0xfb3f3f, 1);
+    // const pointLight2 = new THREE.PointLight(0xfb3f3f, 1);
+    const pointLight2 = new THREE.PointLight(0xfff1b8, 1.5);
     pointLight2.position.x = -8;
-    pointLight2.position.z = 3;
+    pointLight2.position.z = 0;
     pointLight2.add(
       new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0xfb3f3f }))
     );
     scene.add(pointLight2);
 
     //Blue
-    const pointLight3 = new THREE.PointLight(0x0000ff, 1);
+    // const pointLight3 = new THREE.PointLight(0x0000ff, 1);
+    const pointLight3 = new THREE.PointLight(0xfff1b8, 1);
     pointLight3.add(
       new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0x0000ff }))
     );
     pointLight3.position.y = 8;
-    pointLight3.position.z = 3;
+    pointLight3.position.z = 5;
     scene.add(pointLight3);
 
     //Cyan
-    const pointLight4 = new THREE.PointLight(0x00f6ff, 1);
+    // const pointLight4 = new THREE.PointLight(0x00f6ff, 1);
+    const pointLight4 = new THREE.PointLight(0xfff1b8, 1);
     pointLight4.add(
       new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0x00f6ff }))
     );
     pointLight4.position.y = -8;
-    pointLight4.position.z = 3;
+    pointLight4.position.z = 5;
     scene.add(pointLight4);
   };
 
   addObjects = () => {
     const geometryOne = new THREE.BoxBufferGeometry(1, 1, 1);
+
     const materialOne = new THREE.MeshStandardMaterial({
-      color: 0xababab,
-      metalness: 0.5,
-      roughness: 0.8
-    });
-    // const materialTwo = new THREE.MeshPhongMaterial({ shininess: 0.0 });
-    const materialTwo = new THREE.MeshStandardMaterial({
       // color: 0x777777,
-      color: 0xffffff,
-      metalness: 0.97,
-      roughness: 0.3
+      color: 0xc9a825,
+      metalness: 0.92,
+      roughness: 0.25
     });
-
-    const textureLoader = new THREE.TextureLoader();
-
-    const materialThree = new THREE.MeshBasicMaterial({
-      color: 0xffffff
-    });
-    cubeThree = new THREE.Mesh(geometryOne, materialThree);
-    cubeThree.position.x = -1.5;
-    cubeThree.material.map = textureLoader.load(
-      "http://localhost:3000/marble512px.jpg"
-    );
 
     cubeOne = new THREE.Mesh(geometryOne, materialOne);
-    cubeTwo = new THREE.Mesh(geometryOne, materialTwo);
-    cubeTwo.position.x = 1.5;
 
     scene.add(cubeOne);
-    scene.add(cubeTwo);
-    scene.add(cubeThree);
   };
 
   animate = () => {
     requestAnimationFrame(this.animate);
     cubeOne.rotation.x += 0.01;
     cubeOne.rotation.y += 0.01;
-    cubeTwo.rotation.x += 0.01;
-    cubeTwo.rotation.y += 0.01;
-    cubeThree.rotation.x += 0.01;
-    cubeThree.rotation.y += 0.01;
 
     // console.log(renderer.info.render.calls); //add right above the render call
     renderer.render(scene, camera);
